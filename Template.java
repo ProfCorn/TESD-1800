d15ef4cd4b3ec882916a8567b86be8fe8bdd34e2
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2024-08-29 14:06:32 -0600

    Add files via upload

---
+++ Template.java
@@ -0,0 +1,20 @@
+package module_1;
+
+import java.util.*;
+
+/*
+ * Author:  Randall Dexter
+ * Date: 
+ * 
+ * Explanation:
+ * 
+*/
+public class Template {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+	}
+
+}
