9316c211b61e3373bb04aa8474b762669970d6e6
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2024-09-05 14:18:52 -0600

    Update and rename Exercise14_15.java to Chapter_14/Exercise14_15.java

--- Chapter_14/Exercise14_15.java
+++ Chapter_14/Exercise14_15.java
@@ -1,18 +1,29 @@
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.*;
 
 public class Exercise14_15 extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {   
 		// Create a scene and place it in the stage
-		//Pane pane = new Pane();
-		Scene scene = new Scene(new MyPolygon(), 400, 400);
-		primaryStage.setTitle("ShowPolygon"); // Set the stage title
+		StackPane stackPane = new StackPane();
+		stackPane.getChildren().add(new MyPolygon());
+		Text thisText = new Text(20,20,"STOP");
+		thisText.setFill(Color.WHITE);
+		thisText.setFont(Font.font("Times new Roman", FontWeight.NORMAL, 80));
+		stackPane.getChildren().add(thisText);
+		Scene scene = new Scene(stackPane, 400, 400);
+		primaryStage.setTitle("Excercise 14-15"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
 	}
