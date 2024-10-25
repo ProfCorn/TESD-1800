8642e488cdbcfa6fdea796a5323941294a8f63fe
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2024-10-25 11:39:05 -0600

    Update Exercise16_1.java

--- Chapter_16/Exercise16_1.java
+++ Chapter_16/Exercise16_1.java
@@ -9,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -19,28 +20,23 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.*;
 
-public class Exercise15_12ish extends Application {
+public class Exercise16_1 extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {   
 		// Create a scene and place it in the stage
-		StackPane stackPane = new StackPane();
+		BorderPane pane = new BorderPane();
 		Rectangle rectangle = new Rectangle(0,0,25,50);
-		Polygon polygon = new Polygon();
-		polygon.setStroke(Color.BLACK);
-		polygon.setFill(null);
-		Text thisText = new Text(20,20,"STOP");
+		Text thisText = new Text(20,20,"programming is fun");
 		thisText.setFill(Color.BLACK);
-		thisText.setFont(Font.font("Times new Roman", FontWeight.NORMAL, 80));
-		stackPane.getChildren().add(thisText);
-		ObservableList<Double> list = polygon.getPoints();
-		list.clear();
-		double centerX = stackPane.getWidth() / 2, centerY = stackPane.getWidth() / 2;
-		polygon.setRotate(-162);
+		thisText.setFont(Font.font("Times new Roman", FontWeight.NORMAL, 20));
 		rectangle.setFill(Color.BLACK);
-		stackPane.getChildren().add(polygon);
-		stackPane.getChildren().add(rectangle);
-		HBox hbox= new HBox(20);
-		Scene scene = new Scene(stackPane, 400, 400);
+		HBox hBoxForBottom= new HBox(20);
+		Button btLeft = new Button("<=");
+		Button btRight = new Button("=>");
+		hBoxForBottom.getChildren().addAll(btLeft, btRight);
+		pane.setBottom(hBoxForBottom);
+		
+		Scene scene = new Scene(pane, 400, 400);
 		primaryStage.setTitle("Excercise 15-12"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
@@ -56,43 +52,3 @@ public class Exercise15_12ish extends Application {
 	}
 }
 
-class MyPolygon extends Pane {
-	Polygon polygon = new Polygon();
-	private void paint() {
-		// Create a polygon and place polygon to pane
-		
-		//polygon.setStroke(Color.BLACK);
-		//polygon.setFill(null);
-		ObservableList<Double> list = polygon.getPoints();
-		list.clear();
-		double centerX = getWidth() / 2-30, centerY = getHeight() / 2;
-		double radius = Math.min(getWidth(), getHeight()) * 0.4;
-
-		// s represents the number of sides of the shape
-		// Make sure to update this number when necessary
-		int s = 5;
-		// Add points to the polygon list
-		for (int i = 0; i < s; i++) {
-			list.add(centerX + radius * Math.cos(2 * i * Math.PI / s)); 
-			list.add(centerY - radius * Math.sin(2 * i * Math.PI / s));
-		}     
-		polygon.setRotate(-162);
-		getChildren().clear();
-		getChildren().add(polygon); 
-	}
-	
-	@Override
-	public void setWidth(double width) {
-		super.setWidth(width);
-		paint();
-	}
-	
-	@Override
-	public void setHeight(double height) {
-		super.setHeight(height);
-		paint();
-	}
-	public Shape getPolygon() {
-		return polygon;
-	}
-}
