7c99d45640faaa91bb77ff035441f1c91502acc7
ProfCorn <158069625+ProfCorn@users.noreply.github.com>
2025-01-07 14:28:20 -0700

    Update Excercise17_01.java

--- Chapter_17/Excercise17_01.java
+++ Chapter_17/Excercise17_01.java
@@ -8,20 +8,30 @@ import java.util.Random;
 
 
 
-public class Excercise17_01 {
+public class Excercise17_1 {
 	
 	
 	
 	public static void main(String[] args) throws IOException {
-		File IntFile = new File("Exercise17_01.txt");
-		try(RandomAccessFile raf = new RandomAccessFile("Exercise17_01.txt", "rw");){
-			raf.setLength(0);
-			Random random = new Random();
-			for(int i = 0; i<100; i++) {
-				raf.writeInt(random.nextInt(10));
-			}
+		
+		
+		File UsedFile = new File("Exercise17_01.txt");
+		if (UsedFile.exists()) {
+			
+		}
+		FileOutputStream output = new FileOutputStream(UsedFile);
+		Random random = new Random();
+		for(int i = 0; i<100; i++) {
+			output.write(random.nextInt(10));
 		}
 		
+		FileInputStream input = new FileInputStream("Exercise17_01.txt");
+	
+		int value;
+		
+		while ((value = input.read()) != -1) {
+			System.out.print(value + " ");
+		}
 	}
 	
 }
